8080b80c56637329a83514cb9fd7f0b865888d70
Sania <sania2991@rambler.ru>
2022-08-27 00:18:38 +0300

    1.2.3. Frontend__slider-home: finish
    Изменено
      ● slider-overflow  -  уменьшил высоты, чтобы пустота не мешала пролистывать слайды
      ● page-header  -  сделал не прозрачным градиент
      ● slider-img
        ○ position => relative  -  размеры будут универсальны. md (600 x 900), mb-up--sm (1400 x 700, min-h: 400px)
        ○ $slider-img-heights  -  перенёс размеры в `_config.sass`
      ● slider-text (position => absolut)  -  нужно следить за содержимом, чтобы не выходили за границы
    
    Добавлено
      ● gsap  -  для анимации текущего номера слайда

--- app/js/blocks/my-sliders.js
+++ app/js/blocks/my-sliders.js
@@ -1,5 +1,9 @@
+// ██ IMPORT ██
+// Swiper
 import { Swiper, EffectFade, Navigation, Parallax, Pagination, Controller } from 'swiper'
 Swiper.use([EffectFade, Navigation, Parallax, Pagination, Controller])
+// Gsap
+import { gsap, Power2 } from 'gsap'
 
 // document.addEventListener('DOMContentLoaded', () => {
 function initSliderHome() {
@@ -9,12 +13,21 @@ function initSliderHome() {
 		speed: 2400,
 		parallax: true,
 		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		}
+	})
+
+	const swiperText = new Swiper('.slider-text', {
+		loop: false,
+		speed: 2400,
+		pagination: {
 			el: '.slider-pagination-count .total',
 			type: 'custom',
 			renderCustom: function(swiper, current, total) {
 				// let totalRes = total >= 10 ? total : `0${total}`
 				// return totalRes
-				return total
+				return `${total}`;
 			}
 		},
 		// TODO: убрать для slider-text
@@ -22,11 +35,44 @@ function initSliderHome() {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true
-		}
 	})
+
+
+	// █████ Связка слайдов
+	swiperText.controller.control = swiperIMG;
+	swiperIMG.controller.control = swiperText;
+
+	// █████ Slide-change
+	let curnum = document.querySelector('.slider-pagination-count .current');
+
+	swiperText.on('slideChange', function () {
+		let ind = swiperText.realIndex + 1;
+		console.log(ind);
+		gsap.to(curnum, .2, {
+			force3D: true,
+			y: -10,
+			opacity: 0,
+			easy: Power2.easeOut,
+			onComplete: function () {
+				gsap.to(curnum, .1, {
+					force3D: true,
+					y: 10
+				});
+				curnum.innerHTML = ind;
+			}
+			
+		});
+		gsap.to(curnum, .2, {
+			force3D: true,
+			y: 0,
+			opacity: 1,
+			easy: Power2.easeOut,
+			delay: .3
+		})
+
+	});
+
+
 
 }
 
