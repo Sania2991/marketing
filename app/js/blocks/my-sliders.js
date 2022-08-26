2119056be5762d2290fcd0a0c9b9ac949323fac6
Sania <sania2991@rambler.ru>
2022-08-26 07:37:25 +0300

    1.2.2. Frontend__slider-home: шаг 2
    ДОБАВЛЕНО:
    
    Управление
      ● slider-share  -  соц. сети
      ● slider-navigation  -  навигация
      ● slider-pagination-count  -  количество находится внутри!
      ● slider-pagination  -  точки
    
    Слайды
      ● slider-img   -   фоновые изображения
    
    Фитчи
      ● slider-overlay  -  обрезает изображение с помощию SVG
      ● https://svg-art.ru/?p=1114  -  здесь настраивал координаты кривой

--- app/js/blocks/my-sliders.js
+++ app/js/blocks/my-sliders.js
@@ -1,5 +1,5 @@
-import { Swiper, EffectFade, Navigation, Parallax } from 'swiper'
-Swiper.use([EffectFade, Navigation, Parallax])
+import { Swiper, EffectFade, Navigation, Parallax, Pagination, Controller } from 'swiper'
+Swiper.use([EffectFade, Navigation, Parallax, Pagination, Controller])
 
 // document.addEventListener('DOMContentLoaded', () => {
 function initSliderHome() {
@@ -8,10 +8,24 @@ function initSliderHome() {
 		loop: false,
 		speed: 2400,
 		parallax: true,
+		pagination: {
+			el: '.slider-pagination-count .total',
+			type: 'custom',
+			renderCustom: function(swiper, current, total) {
+				// let totalRes = total >= 10 ? total : `0${total}`
+				// return totalRes
+				return total
+			}
+		},
+		// TODO: убрать для slider-text
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		}
 	})
 
 }
